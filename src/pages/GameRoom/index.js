a08990cdb34c111af75aa9f23d859fa3b3aca1fe
Bernardo Bevilaqua <bernardo.bfg@hotmail.com>
2021-09-17 14:10:40 -0300

    primeira versao dos botoes feita

--- src/pages/GameRoom/index.js
+++ src/pages/GameRoom/index.js
@@ -3,26 +3,101 @@ import { useState } from "react";
 import {
     Container,
     TitleArea,
-    GameArea,
-    Score
+    Score,
+    MainArea,
+    RulesBtn,
+    ModalContainer,
+    InnerModal,
+    GameResult,
+    GameOptions,
+    GameOption
 } from "./styles";
 
 import logo from "../../assets/logo.svg";
-
+import imageRules from "../../assets/image-rules.svg";
+import triangle from "../../assets/bg-triangle.svg";
+import scissors from "../../assets/icon-scissors.svg";
+import rock from "../../assets/icon-rock.svg";
+import paper from "../../assets/icon-paper.svg";
 const GameRoom = () => {
     const [score, setScore] = useState(0)
+    const [isRulesOpen, setIsRulesOpen] = useState(false)
+    const [isTimeToPlay, setIsTimeToPlay] = useState(true)
+
+
+    const gameOptions = [
+        {
+            name: "scissors",
+            icon: scissors,
+            beats: "paper",
+            color: ["hsl(39, 89%, 49%) ", "hsl(40, 84%, 53%)"],
+        },
+        {
+            name: "paper",
+            icon: paper,
+            beats: "rock",
+            color: ["hsl(230, 89%, 62%)","hsl(231, 91%, 66%)"],
+        },
+        {
+            name: "rock",
+            icon: rock,
+            beats: "scissors",
+            color: ["hsl(349, 71%, 52%)","hsl(349, 70%, 56%)"],
+        }
+    ]
+
+    const ModalRules = () => {
+        const handleOutsideClick = (e) => {
+            if (e.target.id === "modal")
+                setIsRulesOpen(false)
+        }
+        return (
+            <ModalContainer id="modal" onClick={handleOutsideClick}>
+                <InnerModal>
+                    <button onClick={() => setIsRulesOpen(false)}>
+                    </button>
+                    <img src={imageRules} alt="Rules" />
+                </InnerModal>
+            </ModalContainer>
+        )
+    }
+
     return (
-        <Container>
-            <TitleArea>
-                <img src={logo} alt="logo" />
-                <Score score={score}>
-                    <p>Score</p>
-                    <h1>{score}</h1>
-                </Score>
-            </TitleArea>
-            
-            <GameArea></GameArea>
-        </Container>
+        <>
+            {isRulesOpen && <ModalRules />}
+            <Container>
+                <TitleArea>
+                    <img src={logo} alt="logo" />
+                    <Score score={score}>
+                        <p>Score</p>
+                        <h1>{score}</h1>
+                    </Score>
+                </TitleArea>
+
+                <MainArea>
+                    <RulesBtn
+                        onClick={() => setIsRulesOpen(!isRulesOpen)}
+                    >
+                        Rules
+                    </RulesBtn>
+                    {
+                        isTimeToPlay ?
+                            <GameOptions>
+                                {gameOptions.map((item) => {
+                                    return (
+                                        <GameOption key={item.name} color={item.color}>
+                                            <img src={item.icon} alt={item.name}/>
+                                        </GameOption>
+                                    )
+                                })}
+                            </GameOptions> :
+                            <GameResult>
+
+                            </GameResult>
+                    }
+                </MainArea>
+            </Container>
+        </>
     )
 }
 
--- src/pages/GameRoom/styles.js
+++ src/pages/GameRoom/styles.js
@@ -1,19 +1,19 @@
 import styled from "styled-components";
-
+import triangle from "../../assets/bg-triangle.svg";
 
 export const Container = styled.div`
     width: 100vw;
     height: 100vh;
-    padding: 30px 0; 
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: space-between;
+    justify-content: flex-start;
     background-color:hsl(237, 49%, 15%);
     background-image: radial-gradient(hsl(214, 43%, 35%), hsl(237, 49%, 15%));
 `
 
 export const TitleArea = styled.div`
+    margin-top: 30px;
     width: 600px;
     height: 130px;
     border: 1px solid hsl(217, 16%, 45%);
@@ -43,5 +43,105 @@ export const Score = styled.div`
 
 `
 
-export const GameArea = styled.div`
-    `
+export const MainArea = styled.div`
+    margin-top: 40px;    
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+`
+
+export const RulesBtn = styled.button`
+    background-color: transparent;
+    width: 100px;
+    height: 35px;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 16px;
+    font-weight: 700;
+    color: #fff;
+    border: 1px solid hsl(217, 16%, 45%);
+    &:hover{
+        background-color:hsl(217, 16%, 45%);
+    }
+`
+
+export const ModalContainer = styled.div`
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    bottom: 0;
+    right: 0auto;
+    left: 0;
+    background-color: hsla(0, 0%, 0%, 0.8);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const InnerModal = styled.div`
+    width: 300px;
+    height: 400px;
+    border-radius: 10px;
+    display: flex;
+    background-color: hsl(0, 0%, 100%);
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    padding: 30px;
+
+    button{
+        align-self: flex-end;
+        margin-bottom: 50px;
+        background-color:transparent;
+        cursor: pointer;
+        padding: 15px;
+        &:before,
+        &:after{
+            content: " ";
+            position: absolute;
+            width: 2.5px;
+            height: 24px;
+            background-color: #B5B8C8;
+        }
+        &:before{
+            transform: rotate(45deg);
+        }
+        &:after{
+            transform: rotate(-45deg);
+        }
+    }
+
+`
+export const GameOptions = styled.div`
+    margin-top: 50px;
+    width: 500px;
+    height: 500px;
+    background-image: url(${triangle});
+    background-position: 50% 0;
+    background-repeat: no-repeat;
+`
+
+export const GameOption = styled.button`
+    width: 130px;
+    height: 130px;
+    border-radius: 100%;
+    cursor: pointer;
+    background-color: hsl(0, 0%, 100%);
+    border: 15px solid ${props => props.color[0]};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &:hover{
+        width: 140px;
+        height: 140px;
+        border: 17px solid ${props => props.color[1]};
+        
+    }
+
+`
+
+
+export const GameResult = styled.div``
+
